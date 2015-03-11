f555dd60f5da18b1250421fe52c611c4467c8792
kristiyan_varbanov <kristiyan.varbanov@158ltd.com>
2015-03-11 17:59:55 +0200

    fix google map

--- app/src/main/java/com/example/blizoo/blizoodiplom/CoveringFragment.java
+++ app/src/main/java/com/example/blizoo/blizoodiplom/CoveringFragment.java
@@ -1,42 +1,42 @@
 package com.example.blizoo.blizoodiplom;
 
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.view.View.OnClickListener;
 
-public class CoveringFragment extends Fragment {
+
+public class CoveringFragment extends Fragment implements OnClickListener {
 
     // Boolean key used to prevent always to send data to the server
     private Boolean mSendPostRequest = true;
 
-    // Layout elements
-    private TextView mDestinationAddress, mGpsLocation;
-    private Button mDrawMyRoute;
-    private RelativeLayout mRelativeLayout;
+    //Default debugging tag
+    private static final String TAG = "Conference Destination Fragment";
 
     //Create google maps
     private GoogleMap googleMap;
+
+    //variable which will hold congress destination coordination
+    private LatLng mCongressLocation = new LatLng(42.649324, 23.395420);
+
+
     //Create google maps view, and with him we can show google maps on the screen
     private MapView mMapView;
-    //variable which will hold congress destination coordination
-    private LatLng mCongressLocation = new LatLng(22.231,25.231);
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,30 +45,68 @@ public class CoveringFragment extends Fragment {
                 false);
 
         initializeLayoutsElements(view);
+
         mMapView.onCreate(savedInstanceState);
+
         //call method which will create and initialize google maps
         createMap();
-        //select navigation drawer position which answer to current activity
-    //    ((MainActivity) getActivity()).selectNavigationDrawer(5);
 
         return view;
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
+
         mMapView.onResume();
-        //get position of title from resources and set title to action bar
-       // String[] res = getResources().getStringArray(R.array.menu_items);
-       // getActivity().getActionBar().setTitle(res[5]);
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Initialize logic behind Click Listeners
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        Integer identifierId = view.getId();
+
+        switch (identifierId) {
+
+
+        }
+    }
 
     /**
      * Make initialize of all layout elements and assign click listeners.
-     * */
+     */
     private void initializeLayoutsElements(View view) {
 
         mMapView = (MapView) view.findViewById(R.id.map_view);
@@ -85,29 +123,47 @@ public class CoveringFragment extends Fragment {
         //try to initialize google maps
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
-            googleMap = mMapView.getMap();
+            mMapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    googleMap = map;
+                    googleMap.getUiSettings().setMapToolbarEnabled(false);
+                    addMarkerToMap();
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * Add marker to the map*
+     */
+    private void addMarkerToMap() {
+
         // create and add marker
         try {
             Marker marker = googleMap.addMarker(new MarkerOptions()
-                    .position(mCongressLocation).title("EcommCongress"));
+                    .position(mCongressLocation).title("eCommCongress"));
 
         } catch (Exception exception) {
             // We can't add Google Map Marker
-            Toast.makeText(getActivity(), "Cannot add marker", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), getString(R.string.cannot_add_marker), Toast.LENGTH_LONG).show();
         }
 
-        //set camera position to zoom 10
+        //set camera position to zoom 16
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(mCongressLocation).zoom(16).build();
         //after set camera position get to this zoom level with some animation
         googleMap.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
+
     }
 
 
 }
+
+
 
