f3486e3424de77de97f9b746e8c09b43ba7f0313
kristiyan_varbanov <kristiyan.varbanov@158ltd.com>
2015-03-13 16:36:07 +0200

    add new feathure for google maps

--- app/src/main/java/com/example/blizoo/blizoodiplom/CoveringFragment.java
+++ app/src/main/java/com/example/blizoo/blizoodiplom/CoveringFragment.java
@@ -2,9 +2,11 @@ package com.example.blizoo.blizoodiplom;
 
 
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +21,10 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.view.View.OnClickListener;
@@ -29,19 +33,25 @@ import static android.view.View.OnClickListener;
 public class CoveringFragment extends Fragment {
 
     private static final LatLng LOWER_MANHATTAN = new LatLng(43.216248, 27.921407);
-    private static final LatLng TIMES_SQUARE = new LatLng(43.213770, 27.922638);
+    private static final LatLng TIMES_SQUARE = new LatLng(43.218295, 27.920543);
     private static final LatLng BROOKLYN_BRIDGE = new LatLng(43.218098, 27.931969);
     private static final LatLng Silistra = new LatLng(43.219631, 27.920808);
+    private static final LatLng newPoint = new LatLng(43.217631, 27.120808);
 
     private GoogleMap googleMap;
     private SupportMapFragment mapFragment;
-    private Circle mCircle;
+    private ArrayList<LatLng> arrayPoints = null;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.covering_fragment, container, false);
 
+        arrayPoints = new ArrayList<LatLng>();
+        arrayPoints.add(TIMES_SQUARE);
+        arrayPoints.add(BROOKLYN_BRIDGE);
+        arrayPoints.add(LOWER_MANHATTAN);
+        arrayPoints.add(Silistra);
         createMap();
 
         return view;
@@ -74,16 +84,16 @@ public class CoveringFragment extends Fragment {
         }
         if (googleMap == null) {
             googleMap = mapFragment.getMap();
-            addMarker();
+           // addMarker();
 
             if (googleMap != null) {
-                addLines();
+                addMarker();
             }
         }
     }
 
     private void drawCircle(LatLng position) {
-        double radiusInMeters = 500.0;
+        double radiusInMeters = 30.0;
         int strokeColor = 0xffff0000; //red outline
         int shadeColor = 0x44ff0000; //opaque red fill
 
@@ -95,21 +105,24 @@ public class CoveringFragment extends Fragment {
 
     }
 
+
+
     private void addMarker() {
+        googleMap.clear();
+        PolylineOptions rectOptions = new PolylineOptions().color(Color.BLUE).width(2).geodesic(true);
 
         try {
-            googleMap.addMarker(new MarkerOptions().position(TIMES_SQUARE)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.hub32)));
-            drawCircle(TIMES_SQUARE);
-            googleMap.addMarker(new MarkerOptions().position(BROOKLYN_BRIDGE)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.hub32)));
-            drawCircle(BROOKLYN_BRIDGE);
-            googleMap.addMarker(new MarkerOptions().position(LOWER_MANHATTAN)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.hub32)));
-            drawCircle(LOWER_MANHATTAN);
-            googleMap.addMarker(new MarkerOptions().position(Silistra)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.hub32)));
-            drawCircle(LOWER_MANHATTAN);
+            for (int i = 0; i < arrayPoints.size(); i++) {
+                googleMap.addMarker(new MarkerOptions().position(arrayPoints.get(i))
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.hub32)));
+                drawCircle(arrayPoints.get(i));
+                rectOptions.add(arrayPoints.get(i));
+            }
+            googleMap.addPolyline(rectOptions);
+
+            // move camera to zoom on map
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Silistra,
+                    14));
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -117,16 +130,35 @@ public class CoveringFragment extends Fragment {
         }
     }
 
+    public float getDistanceInMetres(LatLng firstDest, LatLng secondDest) {
+
+        float[] dist = new float[1];
+        double p1 = firstDest.latitude;
+        double p2 = firstDest.longitude;
+        double p3 = secondDest.latitude;
+        double p4 = secondDest.longitude;
+        Location.distanceBetween(p1, p2, p3, p4, dist);
+
+        Log.d("RESULT", Float.toString(dist[0]));
+        return dist[0];
+    }
+
     private void addLines() {
 
-        googleMap
+        PolylineOptions rectOptions = new PolylineOptions().color(Color.BLUE).width(2).geodesic(true);
+
+        for (int i = 0; i < arrayPoints.size(); i++) {
+            rectOptions.add(arrayPoints.get(i));
+        }
+/*        googleMap
                 .addPolyline((new PolylineOptions())
-                        .add(TIMES_SQUARE, BROOKLYN_BRIDGE, LOWER_MANHATTAN,
-                                TIMES_SQUARE,Silistra).width(5).color(Color.BLUE)
-                        .geodesic(true));
+                        .add(rectOptions).width(5).color(Color.BLUE)
+                        .geodesic(true));*/
+        googleMap.addPolyline(rectOptions);
+
         // move camera to zoom on map
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Silistra,
-                10));
+                14));
     }
 
 
